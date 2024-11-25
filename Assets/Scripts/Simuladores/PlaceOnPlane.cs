de82f0731c4d2a23636d0ba25be7eb54e0ebe90a
almixmendez <almendezb2211@gmail.com>
2024-11-24 23:48:41 -0300

    Scripts

--- Assets/Scripts/Simuladores/PlaceOnPlane.cs
+++ Assets/Scripts/Simuladores/PlaceOnPlane.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
-public class NewBehaviourScript : MonoBehaviour
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaceOnPlane : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    GameObject placedPrefab;
+    GameObject spawnedObject;
+
+    ARRaycastManager aRRaycastManager;
+    List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    void Awake()
     {
-        
+        aRRaycastManager = GetComponent<ARRaycastManager>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount == 0)
+            return;
+
+        if (aRRaycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = hits[0].pose;
+
+            if (spawnedObject == null)
+            {
+                spawnedObject = Instantiate(placedPrefab, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                spawnedObject.transform.position = hitPose.position;
+                spawnedObject.transform.rotation = hitPose.rotation;
+            }
+            Vector3 lookPos = Camera.main.transform.position - spawnedObject.transform.position;
+            lookPos.y = 0;
+            spawnedObject.transform.rotation = Quaternion.LookRotation(lookPos);
+        }
     }
 }
